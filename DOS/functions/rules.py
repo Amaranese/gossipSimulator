7bdd37190d51cc340e9aa7d77b7be93308864e86
Amaranese <alejandromaranes@gmail.com>
2022-04-28 11:24:10 +0200

    Create rules.py

---
+++ DOS/functions/rules.py
@@ -0,0 +1,92 @@
+
+def limitGossipWithSamePerson(thisCitizen,other_citizen):
+		krLen = len(thisCitizen['knownRumours'])
+		if(krLen>0):
+			rumourKeys = list(map(int,list(thisCitizen['knownRumours'].keys())))
+			rumourKeys.sort()
+
+			## DON'T CREATE GOSSIP TO THE SAME PERSON YOU JUST CREATED TO 
+			gosIndex = 1
+			for x in range(1,krLen):
+				if(('confidant' in thisCitizen['knownRumours'][str(rumourKeys[-x])]) ):
+					gosIndex = x
+					break
+
+			if('confidant' in thisCitizen['knownRumours'][str(rumourKeys[-gosIndex])]):
+				if(other_citizen['name'] == thisCitizen['knownRumours'][str(rumourKeys[-gosIndex])]['confidant']):
+					return("False")
+
+		return('True')
+					
+
+
+## PULL RULES INFO
+
+# gets rule value
+def getRules(rulesFile,targetVar):
+	f = open(rulesFile, "r")
+	rules = f.read()
+	rules = rules.split(',')
+	for r in rules: 
+		if(r.split(':')[0] == str(targetVar)):
+			return(r.split(':')[1])
+
+	print('Target variable not found in rules file')
+	print('variable is: ' + str(targetVar))
+	exit()
+
+# sets the value 
+def updateRule(rulesFile,targetVar,targetVal):
+	f = open(rulesFile, "r")
+	rules = f.read()
+	rules = rules.split(',')
+	f.close()
+
+	newRules = ""
+	for rule in rules:
+		if(str(targetVar) in str(rule)):
+			updatedRule = str(rule.split(':')[0]) + ":" + str(targetVal)
+			newRules+= str(updatedRule) + ','
+		else:
+			newRules+=str(rule) + ','
+
+	# remove trailing comma
+	newRules = newRules[:-1]
+
+
+	f = open(rulesFile, "w")
+	f.write(newRules)
+	f.close()
+
+def getAllRuleNames(rulesFile):
+	f = open(rulesFile, "r")
+	rules = f.read()
+	rules = rules.split(',')
+	rulesList = []
+	for r in rules: 
+		rulesList.append(str(r.split(':')[0]))
+	return(rulesList)
+
+# Returns rule and value 
+def getFullRules(rulesFile,targetVar):
+	f = open(rulesFile, "r")
+	rules = f.read()
+	rules = rules.split(',')
+	for r in rules: 
+		if(r.split(':')[0] == str(targetVar)):
+			return(r)
+
+	print('Target variable not found in rules file')
+	print('variable is: ' + str(targetVar))
+	exit()
+
+def getRulesSchema(rulesFile,targetVar):
+	f = open(rulesFile, "r")
+	rules = f.read()
+	rules = rules.split(';')
+	for r in rules: 
+		if(r.split(':')[0] == str(targetVar)):
+			return(r)
+
+	print('Target variable not found in rules file')
+	print('variable is: ' + str(targetVar))
